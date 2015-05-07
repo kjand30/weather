4d5fe3db03fef958cda3e0181b61a3c104fc84b6
kjand30 <kjand11@gmail.com>
2015-05-07 17:24:49 -0400

    add css fade in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'results' => 'weather#results'
 
+  get 'search'  => 'weather#search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20150507182843_add_locationto_user.rb
@@ -0,0 +1,4 @@
+class AddLocationtoUser < ActiveRecord::Migration
+  def change
+  end
+end
