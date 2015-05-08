3cd57aa61d87b0090cdc9fc51d0693c3dfbb0e58
Mike Finocchiaro <pushon@onajunket.com>
2015-05-08 18:51:56 -0400

    final 2

--- app/models/weather_api.rb
+++ app/models/weather_api.rb
@@ -14,18 +14,78 @@ class WeatherAPI
     "Accept" => "application/json"
     }
 
-    location_weather = response.body
+    # location_weather = response.body
 
-  end 
 
-  def self.get_weather2(location2)
-      response2 = Unirest.get "https://george-vustrey-weather.p.mashape.com/api.php?location=#{location2}",
-    headers:{
-    "X-Mashape-Key" => "N4oUSJBSeymsh73eOWuHWwHSyjB7p1UmqkzjsnXCc7UMI3jnzD",
-    "Accept" => "application/json"
-    }
 
-    location2_weather = response2.body
+  # def self.get_weather2(location2)
+  #     response2 = Unirest.get "https://george-vustrey-weather.p.mashape.com/api.php?location=#{location2}",
+  #   headers:{
+  #   "X-Mashape-Key" => "N4oUSJBSeymsh73eOWuHWwHSyjB7p1UmqkzjsnXCc7UMI3jnzD",
+  #   "Accept" => "application/json"
+  #   }
+
+  #   location2_weather = response2.body
+
+  [
+      {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      },
+            {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      },
+            {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      },
+      {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      },
+      {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      },
+      {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      },
+      {
+        "day_of_week" => "Fri",
+        "high" => (80..90).to_a.sample,
+        "low" => (70..80).to_a.sample,
+        "high_celsius" => "-17",
+        "low_celsius" => "-17",
+        "condition" => ""
+      }
+    ]
+
 
 
   end
